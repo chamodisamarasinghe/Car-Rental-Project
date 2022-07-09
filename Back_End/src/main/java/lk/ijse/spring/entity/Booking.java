45b837038c34ba80a79e751ccd7311bbabda28d8
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-09 23:45:42 +0530

    Updated booking

--- Back_End/src/main/java/lk/ijse/spring/entity/Booking.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Booking.java
@@ -1,4 +1,25 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@ToString
 public class Booking {
+    @Id
+    private String bookingId;
+    private String vehicleNum;
+    private String NIC;
+    private String drivingLicense;
+    private String location;
+    private String leavingDate;
+    private String returnDate;
 }
