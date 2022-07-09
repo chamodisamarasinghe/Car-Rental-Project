7c2c4a8cfdac95e02321581e2587c091f5bd91ee
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-09 23:42:57 +0530

    Updated admin

--- Back_End/src/main/java/lk/ijse/spring/entity/Admin.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -1,4 +1,22 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@ToString
 public class Admin {
+    @Id
+    private String regId;
+    private String name;
+    private String contactNum;
+    private String email;
 }
