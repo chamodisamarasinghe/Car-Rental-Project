ebd9905a904f848881c838d3f5a967e7bb3bad01
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-09 22:52:52 +0530

    Updated car

--- Back_End/src/main/java/lk/ijse/spring/entity/Car.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Car.java
@@ -1,4 +1,30 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@ToString
 public class Car {
+    @Id
+    private String vehicleNum;
+    private String brand;
+    private int numOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String colour;
+    private String type;
+    private double monthlyRate;
+    private double dailyRate;
+    private String freeMileageDuration;
+    private double freeMileageForThePrice;
+    private double priceForTheExtraKm;
 }
