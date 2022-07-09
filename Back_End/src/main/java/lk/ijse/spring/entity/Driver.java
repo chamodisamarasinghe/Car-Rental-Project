53f7c241357d09767286b6b25aa8f630dec893b1
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-09 22:57:40 +0530

    Updated driver

--- Back_End/src/main/java/lk/ijse/spring/entity/Driver.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Driver.java
@@ -1,4 +1,25 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.ForeignKey;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@ToString
 public class Driver {
+    @Id
+    private String drivingLicense;
+    private String name;
+    private String address;
+    private String contactNum;
+    private String vehicleNum;
+
 }
