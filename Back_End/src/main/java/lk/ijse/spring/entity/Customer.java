f800e647ca8f8f9dc338753542351689f17da34f
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-09 22:53:21 +0530

    Updated customer

--- Back_End/src/main/java/lk/ijse/spring/entity/Customer.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,4 +1,29 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@ToString
 public class Customer {
+    @Id
+    private String NIC;
+    private String passWord;
+    private String drivingLicenseNum;
+    private String address;
+    private String email;
+    private String contactNum;
+
+
+
+
+
 }
