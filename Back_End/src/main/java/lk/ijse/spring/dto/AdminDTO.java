2aa10b2c9132452b11f7fefaafca801030890ae0
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-10 11:23:32 +0530

    Updated dto

--- Back_End/src/main/java/lk/ijse/spring/dto/AdminDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/AdminDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class AdminDTO {
+    private String regId;
+    private String name;
+    private String contactNum;
+    private String email;
 }
--- Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -7,7 +7,7 @@ import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data // apply getters and setters
+@Data
 @ToString
 public class CustomerDTO {
     private String NIC;
