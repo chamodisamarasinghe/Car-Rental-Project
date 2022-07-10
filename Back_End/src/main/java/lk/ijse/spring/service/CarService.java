0f97c7644d5d38781266567bd948ea2de0a261b2
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-10 20:19:56 +0530

    Updated CarService

--- Back_End/src/main/java/lk/ijse/spring/service/CarService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/CarService.java
@@ -1,4 +1,13 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CarDTO;
+
+import java.util.List;
+
 public interface CarService {
+    void saveCar(CarDTO dto);
+    void deleteCar(String vehicleNum);
+    void updateCar(CarDTO dto);
+    CarDTO searchCar(String vehicleNum);
+    List<CarDTO> getAllCar();
 }
--- Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
@@ -1,4 +1,41 @@
 package lk.ijse.spring.service.impl;
 
-public class CarServiceImpl {
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class CarServiceImpl  implements CarService {
+
+
+    @Override
+    public void saveCar(CarDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCar(String vehicleNum) {
+
+    }
+
+    @Override
+    public void updateCar(CarDTO dto) {
+
+    }
+
+    @Override
+    public CarDTO searchCar(String vehicleNum) {
+        return null;
+    }
+
+    @Override
+    public List<CarDTO> getAllCar() {
+        return null;
+    }
 }
