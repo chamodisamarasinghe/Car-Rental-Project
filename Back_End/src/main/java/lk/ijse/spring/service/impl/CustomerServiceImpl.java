2e57efda1b61e57f4bf8573d20df34fb72bb7cf9
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-10 12:54:57 +0530

    Updated CustomerServiceImpl

--- Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,38 @@
 package lk.ijse.spring.service.impl;
 
-public class CustomerServiceImpl {
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public void saveCustomer(CustomerDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String NIC) {
+
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO dto) {
+
+    }
+
+    @Override
+    public CustomerDTO searchCustomer(String NIC) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return null;
+    }
 }
