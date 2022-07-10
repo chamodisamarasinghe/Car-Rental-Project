3651b7527bd7f3ae4d5c638a15cf382f2bb421f5
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-10 11:24:56 +0530

    Updated dto

--- Back_End/src/main/java/lk/ijse/spring/dto/BookingDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/BookingDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class BookingDTO {
+    private String bookingId;
+    private String vehicleNum;
+    private String NIC;
+    private String drivingLicense;
+    private String location;
+    private String leavingDate;
+    private String returnDate;
 }
