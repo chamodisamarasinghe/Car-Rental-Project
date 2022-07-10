452204bb9fea8386ba5c10405586c6841e122f6e
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-10 11:26:10 +0530

    Updated dto

--- Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -1,4 +1,25 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class CarDTO {
+    private String vehicleNum;
+    private String brand;
+    private int numOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String colour;
+    private String type;
+    private double monthlyRate;
+    private double dailyRate;
+    private String freeMileageDuration;
+    private double freeMileageForThePrice;
+    private double priceForTheExtraKm;
 }
