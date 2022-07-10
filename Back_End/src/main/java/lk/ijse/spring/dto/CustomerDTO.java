6345ed18fdd05575f48be1716abf6cc5598ad8dd
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-10 11:22:39 +0530

    Updated dto

--- Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -1,4 +1,19 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // apply getters and setters
+@ToString
 public class CustomerDTO {
+    private String NIC;
+    private String passWord;
+    private String drivingLicenseNum;
+    private String address;
+    private String email;
+    private String contactNum;
 }
--- Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -1,4 +1,13 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDTO dto);
+    void deleteCustomer(String id);
+    void updateCustomer(CustomerDTO dto);
+    CustomerDTO searchCustomer(String id);
+    List<CustomerDTO> getAllCustomers();
 }
