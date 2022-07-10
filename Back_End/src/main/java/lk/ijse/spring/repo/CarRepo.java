f82599aea0b25510983bb86c12256097d9993bfc
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-10 20:20:39 +0530

    Updated CarRepo

--- Back_End/src/main/java/lk/ijse/spring/repo/CarRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/CarRepo.java
@@ -1,4 +1,8 @@
 package lk.ijse.spring.repo;
 
-public interface CarRepo {
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepo extends JpaRepository<Car, String> {
 }
