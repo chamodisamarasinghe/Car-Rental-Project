4dff5f358dbe8cc2816e799daccce8c43ec0b1cf
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-11 17:31:25 +0530

    Updated CustomerServiceImpl

--- Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,12 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -11,73 +16,124 @@ import java.util.List;
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
+    CustomerRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
     @Override
     public void saveCustomer(CustomerDTO dto) {
-
-    }
-
-    @Override
-    public void deleteCustomer(String NIC) {
-
+        if (!repo.existsById(dto.getCustomerId())) {
+            repo.save(mapper.map(dto, Customer.class));
+        } else {
+            throw new RuntimeException("Customer Already Exists");
+        }
     }
 
     @Override
     public void updateCustomer(CustomerDTO dto) {
-
+        if (repo.existsById(dto.getCustomerId())) {
+            repo.save(mapper.map(dto, Customer.class));
+        } else {
+            throw new RuntimeException("No Such Customer To Update");
+        }
     }
 
     @Override
-    public CustomerDTO searchCustomer(String id) {
-        return null;
+    public CustomerDTO searchCustomer(String customerId) {
+        if (repo.existsById(customerId)) {
+            return mapper.map(repo.findById(customerId).get(), CustomerDTO.class);
+        } else {
+            throw new RuntimeException("No Such Customer");
+        }
+    }
+
+    @Override
+    public void deleteCustomer(String customerId) {
+        if (repo.existsById(customerId)) {
+            repo.deleteById(customerId);
+        } else {
+            throw new RuntimeException("No Such Customer To Delete");
+        }
     }
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+        return mapper.map(repo.findAll(), new TypeToken<List<CustomerDTO>>() {
+        }.getType());
     }
 
     @Override
     public boolean findCustomerByUsername(String username) {
-        return false;
+        return repo.findCustomerByUsername(username).isPresent();
     }
 
     @Override
     public boolean findCustomerByPassword(String password) {
-        return false;
+        return repo.findCustomerByPassword(password).isPresent();
     }
 
     @Override
     public CustomerDTO findCustomerByUsernameAndPassword(String username, String password) {
-        return null;
+        return mapper.map(repo.findCustomerByUsernameAndPassword(username, password).get(), CustomerDTO.class);
     }
 
     @Override
     public String generateCustomerId() {
-        return null;
+        String lastId = repo.generateCustomerId();
+        String id = "";
+
+        if (lastId != null) {
+            int tempId = Integer.parseInt(lastId.split("-")[1]);
+            tempId = tempId + 1;
+            if (tempId <= 9) {
+                id = "C00-000" + tempId;
+            } else if (tempId <= 99) {
+                id = "C00-00" + tempId;
+            } else if (tempId <= 999) {
+                id = "C00-0" + tempId;
+            } else if (tempId <= 9999) {
+                id = "C00-" + tempId;
+            }
+        } else {
+            id = "C00-0001";
+        }
+        return id;
     }
 
     @Override
     public void updateCustomerStatus(String id) {
-
+        if (repo.existsById(id)) {
+            repo.updateCustomerStatus(id);
+        } else {
+            throw new RuntimeException("Customer Not Found");
+        }
     }
 
     @Override
     public List<CustomerDTO> getAllPendingCustomers() {
-        return null;
+        return mapper.map(repo.findPendingCustomers(), new TypeToken<List<CustomerDTO>>() {
+        }.getType());
     }
 
     @Override
     public List<CustomerDTO> getAllAcceptedCustomers() {
-        return null;
+        return mapper.map(repo.findAcceptedCustomers(), new TypeToken<List<CustomerDTO>>() {
+        }.getType());
     }
 
     @Override
     public void uploadCustomerImages(String nicfPath, String nicbPath, String licenceImgPath, String id) {
-
+        if (repo.existsById(id)) {
+            repo.updateCustomerFilePaths(nicfPath, nicbPath, licenceImgPath, id);
+        } else {
+            throw new RuntimeException("Customer Not Found");
+        }
     }
 
     @Override
     public int getCountOfCustomersRegistered() {
-        return 0;
+        return repo.countByCustomerId();
     }
 }
