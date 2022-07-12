2723b7e8c8742e856b1adb4397a81c315a9759ec
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-12 18:47:24 +0530

    Updated CarController

--- Back_End/src/main/java/lk/ijse/spring/controller/CarController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -9,41 +9,86 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("api/v1/car")
 @CrossOrigin
 public class CarController {
     @Autowired
-    CarService carService;
+    CarService service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil getAllCar() {
-        return new ResponseUtil(200,"Ok",carService.getAllCar());
+    public ResponseUtil getAllCars() {
+        return new ResponseUtil(200, "Ok", service.getAllCars());
     }
 
-    @ResponseStatus(HttpStatus.CREATED) //201
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil saveCar(@ModelAttribute CarDTO car) {
-        carService.saveCar(car);
-        return new ResponseUtil(200,"Save",null);
+    public ResponseUtil saveCar(CarDTO dto) {
+        service.saveCar(dto);
+        return new ResponseUtil(200, "Saved", null);
     }
-
 
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil updateCar(@RequestBody CarDTO car) {
-        carService.updateCar(car);
-        return new ResponseUtil(200,"Updated",null);
+    public ResponseUtil updateCar(@RequestBody CarDTO dto) {
+        service.updateCar(dto);
+        return new ResponseUtil(200, "Updated", null);
     }
 
-    @DeleteMapping(params = {"vehicleNum"},produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil deleteCar(@RequestParam String vehicleNum) {
-        carService.deleteCar(vehicleNum);
-        return new ResponseUtil(200,"Deleted",null);
+    @DeleteMapping(params = {"registrationNo"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCar(@RequestParam String registrationNo) {
+        service.deleteCar(registrationNo);
+        return new ResponseUtil(200, "deleted", null);
     }
 
-    @GetMapping(path = "/vehicleNum}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil searchCar(@PathVariable String vehicleNum) {
-        return new ResponseUtil(200,"Ok",carService.searchCar(vehicleNum));
+    @GetMapping(path = "/{registrationNo}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCar(@PathVariable String registrationNo) {
+        return new ResponseUtil(200, "Ok", service.searchCar(registrationNo));
+    }
+
+    @PutMapping(path = "/updateCarStatus/{registrationNO}/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCarStatus(@PathVariable String registrationNO, @PathVariable String status) {
+        service.updateCarStatus(registrationNO, status);
+        return new ResponseUtil(200, "Ok", null);
+    }
+
+    @GetMapping(path = "/getByStatus/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCarsByStatus(@PathVariable String status) {
+        return new ResponseUtil(200, "Ok", service.getAllCarsByStatus(status));
+    }
+
+    @GetMapping(path = "/count/{status}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getCountOfCarsByStatus(@PathVariable String status) {
+        return new ResponseUtil(200, "Ok", service.getCountOfCarsByStatus(status));
+    }
+
+    @PutMapping(path = "/up/{registrationID}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil uploadImagesAndPath(@RequestPart("frontImg") MultipartFile frontImg, @RequestPart("backImg") MultipartFile backImg, @RequestPart("interImg") MultipartFile interImg, @RequestPart("sideImg") MultipartFile sideImg, @PathVariable String registrationID) {
+        try {
+            String projectPath = String.valueOf(new File("H:/GDSE58/2nd Sem Final Project/Back_End/src/main/resources/static/images"));
+            File uploadsDir = new File(projectPath + "/Cars");
+            uploadsDir.mkdir();
+            frontImg.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + frontImg.getOriginalFilename()));
+            backImg.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + backImg.getOriginalFilename()));
+            interImg.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + interImg.getOriginalFilename()));
+            sideImg.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + sideImg.getOriginalFilename()));
+
+            String frontImgPath = projectPath + "/Cars/" + frontImg.getOriginalFilename();
+            String backImgPath = projectPath + "/Cars/" + backImg.getOriginalFilename();
+            String interImgPath = projectPath + "/Cars/" + interImg.getOriginalFilename();
+            String sideImgPath = projectPath + "/Cars/" + sideImg.getOriginalFilename();
+
+            service.updateCarFilePaths(frontImgPath, backImgPath, interImgPath, sideImgPath, registrationID);
+
+            return new ResponseUtil(200, "Uploaded", null);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseUtil(500, "Error", null);
+        }
     }
 }
