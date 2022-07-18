1ca1964c14df2f0397ccb08cb7d712b89ac354c7
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-18 17:58:35 +0530

    Added files

---
+++ Front_End/front-end/src/app/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import '../App.css';
+import {Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+      <Routes>
+      // {/*<Route exact path='/' element={<HomePage/>}/>*/}
+      // <Route exact path='posts' element={<Posts/>}/>
+      // <Route path='login' element={<Login/>}/>
+      // <Route path='flex-layout' element={<FlexBoxLayout/>}/>
+      // <Route path='grid-layout' element={<GridLayout/>}/>
+      // <Route path="*" element={<NotFound/>}/>
+      // <Route path="customer" element={<Customer/>}/>
+      // <Route path="item" element={<Item/>}/>
+      // <Route exact path='/' element={<DashBoard/>}/>
+  </Routes>
+
+
+);
+}
+
+export default App;
